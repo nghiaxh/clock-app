a8c2ede9079f62f91d14f3a565227eb94ad3c656
Nghia Hoang <nghiahoang880@gmail.com>
2023-11-29 15:51:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateClockTime() {
+  const updateClockTimeNow = new Date();
+  const hours = updateClockTimeNow.getHours().toString().padStart(2, 0);
+  const minutes = updateClockTimeNow.getMinutes().toString().padStart(2, 0);
+  const seconds = updateClockTimeNow.getSeconds().toString().padStart(2, 0);
+  const getTime = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock-time").textContent = getTime;
+}
+
+updateClockTime();
+setInterval(updateClockTime, 1000);
+
+function updateClockDate() {
+  const updateClockDateNow = new Date();
+    const getDate = updateClockDateNow.toLocaleDateString( 'en-US' );
+  document.getElementById("clock-date").textContent = getDate;
+}
+
+updateClockDate();
+setInterval(updateClockDate, 1000);
